620d8c6cabbde2fde1a2bdd538cbe1bcf6077fe7
JckD <c16335383@mydit.ie>
2021-03-13 02:26:55 +0000

    changed full page to class

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,2 +1,7 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './src/styles/global.css'
+
+
+function afterLoad(origin, destination, direction) {
+    console.log("After load: " + destination.index);
+}
--- src/components/fullpage.js
+++ src/components/fullpage.js
@@ -1,37 +1,97 @@
 import React from "react";
 import ReactFullpage from '@fullpage/react-fullpage';
 import Header from '../components/header'
+import anime from 'animejs/lib/anime.es.js';
 
 
-export default function FullPage() {
-    return ( 
+class FullPage extends React.Component {
+    onLeave(origin, destination, direction) {
+      console.log("Leaving section " + origin.index);
+    }
+    afterLoad(origin, destination, direction) {
+      console.log("After load: " + destination.index);
+    }
+    render() {
+      return (
         <ReactFullpage
-
-            //full page options
-            licenseKey = {process.env.FP_LICENSE}
-            scrollingSpeed = {1000}
-
-            render={({ state, fullpageApi }) => {
-                return (
-                    <ReactFullpage.Wrapper>
-                        <div className="section">
-                            <Header text='Jack Doyle'/>
-                            {/* <Button  onClick={() => fullpageApi.moveSectionDown()}>
-                                Click me to move down
+          anchors={["JackDoyle", "AboutMe", "Projects", "Contact"]}
+          
+          scrollOverflow={true}
+          onLeave={this.onLeave.bind(this)}
+          afterLoad={this.afterLoad.bind(this)}
+          render={({ state, fullpageApi }) => {
+            return (
+              <div id="fullpage-wrapper">
+                <div className="section active">
+                    <Header text='Jack Doyle'/>
+                             {/* <Button  onClick={() => fullpageApi.moveSectionDown()}>
+                               Click me to move down
                             </Button> */}
-                        </div>
-                        <div className="section">
-                            <Header text='About Me'/>
-                        </div>
-                        <div className="section" >
-                            <Header text='Projects'/>
-                        </div>
-                        <div className="section">
-                            <Header text='Contact'/>
-                        </div>
-                    </ReactFullpage.Wrapper>
-                );
-            }}
+                </div>
+                <div className="section">
+                    <Header text='About Me'/>
+                </div>
+                <div className="section" >
+                        <Header text='Projects'/>
+                </div>
+                <div className="section">
+                        <Header text='Contact'/>
+                </div>
+              </div>
+            );
+          }}
         />
-    )};
+      );
+    }
+  }
+
+  export default FullPage;
+
+
+
+// export default function FullPage() {
+
+    
+//     return ( 
+//         <ReactFullpage
+
+//             //full page options
+
+//             licenseKey = {process.env.FP_LICENSE}
+//             scrollingSpeed = {1000}
+//             anchors={["JackDoyle", "AboutMe", "Projects", "Contact"]}
+//             onLeave={(console.log('onleave'))}
+//             afterLoad={(console.log('cum'))}
+
+//             render={({ state, fullpageApi }) => {
+//                 return (
+//                     <ReactFullpage.Wrapper>
+//                         
+//                     </ReactFullpage.Wrapper>
+//                 );
+//             }}
+//         />
+//     )
+    
+// };
+
+// function onLeave(origin, destination, direction) {
+//     console.log("Leaving section " + origin.index);
+// }
+// function afterLoad(origin, destination, direction) {
+// console.log("After load: " + destination.index);
+// }
+
+
+
+ 
+// var animation = anime({
+//     targets: 'h1',
+//     translateX: 20,
+//     delay: 1000,
+//     easing: 'easeInOutCirc',
+   
+// })
+
+
 
