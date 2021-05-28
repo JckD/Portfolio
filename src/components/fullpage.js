8d1ce0d33dbeaf6336c519b01f8c1db65c8388d4
JckD <c16335383@mydit.ie>
2021-05-28 18:24:54 +0100

    started contact section

--- src/components/fullpage.js
+++ src/components/fullpage.js
@@ -93,6 +93,8 @@ class FullPage extends React.Component {
 
     })
 
+
+
     // var subheaderAnimation = anime({
     //   targets: subheader,
     //   translateX: [0, 240],
@@ -111,6 +113,7 @@ class FullPage extends React.Component {
     //   easing : 'easeInOutQuad'
     // })
   }
+
 
 
   
@@ -245,9 +248,9 @@ class FullPage extends React.Component {
                       <Col>
                       <div className="heroFooter">
                        
-                      <svg  xmlns="http://www.w3.org/2000/svg" width="64" height="64" class="bi bi-mouse-fill" viewBox="0 0 16 16">
-                        <path className="scroller" onClick={() => fullpageApi.moveSectionDown()} d="M3 5a5 5 0 0 1 10 0v6a5 5 0 0 1-10 0V5zm5.5-1.5a.5.5 0 0 0-1 0v2a.5.5 0 0 0 1 0v-2z"/>
-                      </svg>
+                        <svg  xmlns="http://www.w3.org/2000/svg" width="64" height="64" class="bi bi-mouse-fill" viewBox="0 0 16 16">
+                          <path className="scroller" onClick={() => fullpageApi.moveSectionDown()} d="M3 5a5 5 0 0 1 10 0v6a5 5 0 0 1-10 0V5zm5.5-1.5a.5.5 0 0 0-1 0v2a.5.5 0 0 0 1 0v-2z"/>
+                        </svg>
                       
                         
                         </div>
@@ -283,7 +286,7 @@ class FullPage extends React.Component {
                           <h3>Education</h3>
                           <p>
                             I am a recent graduate from BSc Computre Science (Infrastucture)
-                            in the <a href='https://www.tudublin.ie/study/undergraduate/courses/computer-science-infrastructure-tu857/'>Technological University of Dublin</a> achieving a 2.1 award.
+                            in the <a className="link" href='https://www.tudublin.ie/study/undergraduate/courses/computer-science-infrastructure-tu857/'>Technological University of Dublin</a> achieving a 2.1 award.
                             I really enjoyed opportunity to get an outlook in different technologies and learn about them in a sctructured way
                           </p>
                           <p>
@@ -297,7 +300,7 @@ class FullPage extends React.Component {
                           </p>
                           <p>
                             I am currently most interested by the Web and Web Tecnologies. I explore this by taking
-                            on my own personal projects in my free time, check them out in the next <a onClick={() => fullpageApi.moveSectionDown()} style={{color: '#a3f7bf', cursor: "pointer"}}>section</a>.
+                            on my own personal projects in my free time, check them out in the next <a className="link" onClick={() => fullpageApi.moveSectionDown()}>section</a>.
                           </p>
                           <hr />
                           <h3>Interests</h3>
@@ -331,27 +334,36 @@ class FullPage extends React.Component {
                         <Row>
                           <div className='cardDeck' id='cardDeck'>
                             <ProjectCard />                         
-
-                         
                         </div>
-                        </Row>
-                        
-                        
-                      </Col>
-                   
+                        </Row>          
+                      </Col>     
                     </Row>
                   </div>
                 </div>
                 <div className="section  sectionBackground">
                   <div className="contentDiv">
                   <Row>
-                      <Col>
-                     
+                      <Col sm={true}>
                           <div className="sectionHeader">
                             <Header text='Contact'/>
                             <SubHeader line1='Get in touch'/>
-                          </div>
-                       
+                          </div>   
+                      </Col>
+                      <Col sm={true}>
+                        <div style={{height : 500}}>
+                          <div className="contactCard">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#29a19c" class="bi bi-envelope-fill" viewBox="0 0 16 16">
+                              <path d="M.05 3.555A2 2 0 0 1 2 2h12a2 2 0 0 1 1.95 1.555L8 8.414.05 3.555zM0 4.697v7.104l5.803-3.558L0 4.697zM6.761 8.83l-6.57 4.027A2 2 0 0 0 2 14h12a2 2 0 0 0 1.808-1.144l-6.57-4.027L8 9.586l-1.239-.757zm3.436-.586L16 11.801V4.697l-5.803 3.546z"/>
+                            </svg>
+                            <a id="email" className="contact">jackp.doyle1@gmail.com</a>     
+                            <hr/>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#29a19c" class="bi bi-linkedin" viewBox="0 0 16 16">
+                              <path d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854V1.146zm4.943 12.248V6.169H2.542v7.225h2.401zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248-.822 0-1.359.54-1.359 1.248 0 .694.521 1.248 1.327 1.248h.016zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016a5.54 5.54 0 0 1 .016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225h2.4z"/>
+                            </svg>
+                            <a id="linkedIn" className="contact" href="https://www.linkedin.com/in/jack-p-doyle/">LinkedIn</a>
+                        </div>
+                        </div>
+                        
                       </Col>
                     </Row>
                     
